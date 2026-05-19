856ff69faad0557718a9fb8bb10dab90a404ce74
Sakshi Chilla <chillasakshi2@gmail.com>
2026-05-19 21:18:58 +0530

    Add files via upload

---
+++ string_slicing.py
@@ -0,0 +1,9 @@
+name = "rahul sharma"
+print(name[0:12:1])  #rahul sharma
+print(name[11:0:-1]) #amrahs luha
+print(name[0:-1:1])  #rahul sharm
+print(name[11:-13:-1]) #amrahs luhar
+print(name[-12:12:1]) #rahul sharma
+print(name[-1:0:-1]) #amrahs luha
+print(name[-12:-1:1]) #rahul sharm
+print(name[-1:-13:-1]) #amrahs luhar
