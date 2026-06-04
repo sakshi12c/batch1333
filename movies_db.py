ec4ff32cb6487fb02d0eabccb26c12bf4899defe
Sakshi Chilla <chillasakshi2@gmail.com>
2026-06-04 19:32:28 +0530

    Add files via upload

---
+++ movies_db.py
@@ -0,0 +1,9 @@
+movies_db = {
+    "deool band 2": ["Pravin Tarade", "Mohan Joshi", "Prasad Oak","Snehal Tarade","Gashmeer Mahajani"],
+    "raja shivaji": ["Riteish Deshmukh", "Genelia Deshmukh", "Sanjay Dutt","Abhishek Bachchan","Vidya Balan"],
+    "animal ": ["Ranbir Kapoor", "Anil Kapoor", "Rashmika Mandanna","Triptii Dimri","Bobby Deol"],
+    "tare zameen par 2": ["Genelia Deshmukh", "Aamir Khan", "Tisca Chopra","Vipin Sharma","Tanay Chheda"],
+    "dhuranadhar": ["Ranveer Singh", "Sara Arjun", "Sanjay Dutt","Akshaye Khanna","R. Madhavan"]
+}
+for movie, cast in movies_db.items():
+    print(movie, "->", cast)
